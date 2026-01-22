fb4a1c514591421284d2f2e1683b8e1ac23db89d
5star55 <tr6845841@gmail.com>
2026-01-22 17:34:00 +0100

    Styles usernames

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Card(props) {
   return (
     <div className="bg-white py-10 m-5 rounded-2xl">
-        <h1 className='pb-10'>{props.name}</h1>
+        <h1 className='pb-10 font-extrabold font-mono text-2xl'>{props.name}</h1>
         <h2>id:{props.id}</h2>
         <h2>email: {props.email}</h2>
         <button className="mt-10 bg-amber-600 rounded-lg px-3 py-1" onClick={()=> props.onClick()}>{props.button}</button>
